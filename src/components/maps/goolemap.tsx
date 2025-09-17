7a5220c97893c611ebfbca9589ed8a30bbfef4c9
Carson McNeill <carsonmcneill2004@gmail.com>
2025-09-16 21:12:03 -0400

    Update goolemap.tsx

--- src/components/maps/goolemap.tsx
+++ src/components/maps/goolemap.tsx
@@ -15,7 +15,7 @@ const gtCampus = { lat: 33.7780, lng: -84.3980 };
  * Call this Google Maps component to display google map every missing item
  * Pins to each item will be clickable --> displays image and info about item
  * 
- * @param props height: height of component, width: width of component
+ * @param props height: height of component, width: width of component. This should be the same syntax as CSS / Tailwind CSS
  * @returns View of Google Map
  */
 export default function GoogleMap(props: { height: any; width: any; }) {
