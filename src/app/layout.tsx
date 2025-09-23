0d140ac77a0c7a752c82eb6d9569813d6a9b3fc1
hu-lisa <lisahu.kaboom@gmail.com>
2025-09-23 18:34:06 -0400

    Added navbar component and imported it into layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { dbConnect } from "@/lib/mongo";
+import Navbar from '../components/navigation/Navbar';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default async function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                <Navbar />
                 {children}
             </body>
         </html>
