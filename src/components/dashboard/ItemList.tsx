8bec557537ed1cf085dc5bd7a09c18a4cd934e80
eminyoung <minyoung.emily@gmail.com>
2025-10-11 02:55:39 -0400

    Add basic layout of LostItemCard + prettier doing its formatting thing

--- src/components/dashboard/ItemList.tsx
+++ src/components/dashboard/ItemList.tsx
@@ -1,14 +1,27 @@
-import { Item } from '@/model/Item'
-import React from 'react'
-import FoundItemCard from './FoundItemCard'
-import LostItemCard from './LostItemCard'
+import { Item } from "@/model/Item";
+import React from "react";
+import FoundItemCard from "./FoundItemCard";
+import LostItemCard from "./LostItemCard";
 
-const ItemList = ({ items, lostItemsSelected }: { items: Item[], lostItemsSelected: boolean | null }) => {
-    return (
-        <div className='m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 p-5 gap-4'>
-            {lostItemsSelected ? items.map((item: Item) => (<LostItemCard key={item.title} item={item} />)) : items.map((item: Item) => (<FoundItemCard key={item.title} item={item} includeMapLink={true}/>))}
-        </div>
-    )
-}
+const ItemList = ({
+  items,
+  lostItemsSelected,
+}: {
+  items: Item[];
+  lostItemsSelected: boolean | null;
+}) => {
+  return (
+    <div className="m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 p-5 gap-4">
+      {/* {lostItemsSelected ? items.map((item: Item) => (<LostItemCard key={item.title} item={item} />)) : items.map((item: Item) => (<FoundItemCard key={item.title} item={item} includeMapLink={true}/>))} */}
+      {lostItemsSelected ? (
+        <LostItemCard />
+      ) : (
+        items.map((item: Item) => (
+          <FoundItemCard key={item.title} item={item} includeMapLink={true} />
+        ))
+      )}
+    </div>
+  );
+};
 
-export default ItemList
+export default ItemList;
--- src/components/dashboard/LostItemCard.tsx
+++ src/components/dashboard/LostItemCard.tsx
@@ -1,12 +1,46 @@
-import { Item } from '@/model/Item'
-import React from 'react'
+import { Item } from "@/model/Item";
+import Image from "next/image";
+import React from "react";
 
-const LostItemCard = ({item}:{item:Item}) => {
+// const LostItemCard = ({ item }: { item: Item }) => {
+//   return (
+//     <div>
+//       <a href={"item/" + item._id}>
+//         <h2>{item.title}</h2>
+//       </a>
+//     </div>
+//   );
+// };
+
+// export default LostItemCard;
+
+function UserInfo() {
   return (
-    <div>
-      
+    <div className="flex flex-row items-center gap-2">
+      <Image
+        src="/default-icon.svg"
+        alt="User avatar"
+        width={30}
+        height={30}
+        className="relative overflow-hidden rounded-full"
+      />
+      <p className="text-xs">Username</p>
     </div>
-  )
+  );
 }
 
-export default LostItemCard
+export default function LostItemCard() {
+  return (
+    <div className="rounded-lg w-full flex flex-col shadow p-3 gap-4">
+      <div className="flex flex-row items-center gap-7">
+        <UserInfo />
+        <p className="text-xs">Date/time Here</p>
+      </div>
+      <div>
+        <h2 className="text-xl font-bold">Item Name</h2>
+        <p className="text-sm">Item Description</p>
+      </div>
+      <p>Contact: email/phone/etc. here</p>
+    </div>
+  );
+}
