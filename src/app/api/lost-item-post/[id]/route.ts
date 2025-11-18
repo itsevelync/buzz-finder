dcf9645e19bc8478e1636992c3b2b5b71a846510
eminyoung <minyoung.emily@gmail.com>
2025-11-18 16:34:12 -0500

    fix issue of username and pfp not showing within lost item page

--- src/app/api/lost-item-post/[id]/route.ts
+++ src/app/api/lost-item-post/[id]/route.ts
@@ -9,7 +9,7 @@ export async function GET(
   try {
     await dbConnect();
     const { id } = await params;
-    const item = await LostItem.findById(id);
+    const item = await LostItem.findById(id).populate("user", "username image");
 
     if (!item) {
       return NextResponse.json(
@@ -29,32 +29,35 @@ export async function GET(
 }
 
 export async function DELETE(
-    request: NextRequest,
-    { params }: { params: Promise<{ id: string }> }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    try {
-        await dbConnect();
-        const { id } = await params;
+  try {
+    await dbConnect();
+    const { id } = await params;
 
-        const item = await LostItem.findByIdAndUpdate(id, { deletedAt: new Date() },
-            { new: true });
+    const item = await LostItem.findByIdAndUpdate(
+      id,
+      { deletedAt: new Date() },
+      { new: true }
+    );
 
-        if (!item) {
-            return NextResponse.json(
-                { message: "Lost item post not found" },
-                { status: 404 }
-            );
-        }
-
-        return NextResponse.json(
-            { message: "Lost item post marked as deleted", item },
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error("Error deleting lost item post:", error);
-        return NextResponse.json(
-            { message: "Internal server error" },
-            { status: 500 }
-        );
+    if (!item) {
+      return NextResponse.json(
+        { message: "Lost item post not found" },
+        { status: 404 }
+      );
     }
+
+    return NextResponse.json(
+      { message: "Lost item post marked as deleted", item },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting lost item post:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
