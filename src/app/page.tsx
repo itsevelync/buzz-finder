ff8f450c6da86e735dcebd659cc5c98c671bcc7d
Evelyn Chen <evelynchen555@gmail.com>
2026-06-20 20:08:56 -0400

    Metadata update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
 import { redirect } from "next/navigation";
-
-export const metadata: Metadata = {
-    title: "BuzzFinder",
-    description:
-        "Lost something? Found something? BuzzFinder helps you report, map, and track lost and found items in your community to quickly reunite them with owners.",
-};
 
 export default async function Home() {
     redirect("map");
